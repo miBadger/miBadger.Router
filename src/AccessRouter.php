81e44787c56214ef052dd6f4cb5b75c914d12254
Berend Baas <berendbaas@gmail.com>
2019-08-14 13:35:22 +0900

    Added access router

---
+++ src/AccessRouter.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace miBadger\Router;
+
+use miBadger\Http\ServerRequest;
+use miBadger\Http\ServerResponse;
+use miBadger\Http\ServerResponseException;
+
+class AccessRouter
+{
+	private $routes; // $routes[method] = (route, pattern, callable, permission)
+
+	private $basePath;
+
+	private $user;
+
+	public function __construct(PermissionCheckable $user = null, $basePath = '')
+	{
+		$this->user = $user;
+		$this->routes = [];
+		$this->basePath = $basePath;
+	}
+	/**
+	 * Returns the base path.
+	 *
+	 * @return string the base path.
+	 */
+	public function getBasePath()
+	{
+		return $this->basePath;
+	}
+
+	/**
+	 * Set the base path.
+	 *
+	 * @param string $basePath
+	 * @return $this
+	 */
+	public function setBasePath($basePath)
+	{
+		$this->basePath = $basePath;
+
+		return $this;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getIterator()
+	{
+		return new \RecursiveArrayIterator($this->routes);
+	}
+
+	/**
+	 * Returns the number of key-value mappings in the router map.
+	 *
+	 * @return int the number of key-value mappings in the router map.
+	 */
+	public function count()
+	{
+		$result = 0;
+
+		foreach ($this->routes as $routes) {
+			$result += count($routes);
+		}
+
+		return $result;
+	}
+
+	public function add(string $method, string $route, $permission, callable $callable)
+	{
+		$entry = (object)[
+			'route' => $route,
+			'pattern' => $this->createPattern($route),
+			'permission' => $permission,
+			'callable' => $callable
+		];
+
+		if (!array_key_exists($method, $this->routes)) {
+			$this->routes[$method] = [];
+		}
+
+		$this->routes[$method][] = $entry;
+	}
+
+	/**
+	 * Creates a regex-enabled pattern from the route
+	 *
+	 * @param string $route
+	 * @return string the pattern string.
+	 */
+	private function createPattern(string $route)
+	{
+		return '|^' . preg_replace('|\{[^\}]+\}|', '([^\/]+)', $route) . '$|';
+	}
+
+	/**
+	 * Removes the mapping for the specified route from the router map if present.
+	 *
+	 * @param string $method
+	 * @param string $route
+	 * @return null
+	 */
+	public function remove(string $method, string $route, $permission)
+	{
+		if (!array_key_exists($method, $this->routes)) {
+			return;
+		}
+
+		foreach ($this->routes[$method] as $key => $entry) {
+			if ($entry->route == $route && $entry->permission == $permission) {
+				unset($this->routes[$method][$key]);
+			}
+		}
+
+		if (count($this->routes[$method]) == 0) {
+			unset($this->routes[$method]);
+		}
+	}
+
+	/**
+	 * Removes all of the mappings from the router map.
+	 *
+	 * @return null
+	 */
+	public function clear()
+	{
+		$this->routes = [];
+	}
+
+
+	/**
+	 * Returns the result of the given route's callable.
+	 *
+	 * @param string|null $method = new ServerRequest()->getMethod()
+	 * @param string|null $route = new ServerRequest()->getUri()->getPath()
+	 * @return mixed the result of the given route's callable.
+	 * @throws ServerResponseException
+	 */
+	public function resolve($method = null, $route = null)
+	{
+		$serverRequest = new ServerRequest();
+
+		if ($method === null) {
+			$method = $serverRequest->getMethod();
+		}
+
+		if ($route === null) {
+			$route = $serverRequest->getUri()->getPath();
+		}
+
+		if ($this->basePath !== '' && strpos($route, $this->basePath, 0) === 0) {
+			$route = substr($route, strlen($this->basePath));
+		}
+
+		return $this->callCallable($this->getCallable($method, $route));
+	}
+
+	/**
+	 * Returns the result of the callable.
+	 *
+	 * @param array the callable and the route matches.
+	 * @return mixed the result the callable.
+	 */
+	private function callCallable($callable)
+	{
+		return call_user_func_array($callable[0], $callable[1]);
+	}
+
+	public function getMatchingRoutes($method, $route)
+	{
+		if (!array_key_exists($method, $this->routes)) {
+			throw new ServerResponseException(new ServerResponse(404));
+		}
+
+		$matchedRoutes = [];
+
+		foreach ($this->routes[$method] as $entry) {
+			if (preg_match($entry->pattern, $route, $matches) > 0) {
+				// At this point the route is matched
+				array_shift($matches);
+				$matchedRoutes[] = [$route, $entry, $matches];
+			}
+		}
+
+		if (count($matchedRoutes) == 0) {
+			throw new ServerResponseException(new ServerResponse(404));		
+		}
+
+		return $matchedRoutes;
+	}
+
+	protected function getCallable($method, $route)
+	{
+		// Check if permissions are alright
+		$matchedRoutes = $this->getMatchingRoutes($method, $route);
+
+		foreach ($matchedRoutes as $match) {
+			$route = $match[0];
+			$entry = $match[1];
+			$matches = $match[2];
+
+			if ($entry->permission === null 
+				|| ($entry->permission !== null && $this->user !== null && $this->user->hasPermission($entry->permission)))
+			{
+				return [$entry->callable, $matches];
+			}
+
+		}
+
+		throw new ServerResponseException(new ServerResponse(401));
+	}
+
+	/**
+	 * Returns the cleaned ({token} => %s) permission map for the router 
+	 * (associative map from route to the set of permissions that have access to that route), 
+	 * 		null if a route has wildcard permissions
+	 */
+	public function makeGETRoutePermissionsMap()
+	{
+		$outputMap = [];
+
+		foreach ($this->routes["GET"] as $entry) {
+
+			// Extract clean route by replacing every slug occurrence with %s
+			$slugFreeRoute = preg_replace('/({.*?})/', '%s', $entry->route);
+
+			if ($entry->permission === null) {
+				$outputMap[$slugFreeRoute] = null;	
+				continue;
+			}
+
+			if (!isset($outputMap[$slugFreeRoute])) {
+				$outputMap[$slugFreeRoute] = [];
+			}
+
+			$outputMap[$slugFreeRoute][] = $entry->permission;
+		}
+
+		return $outputMap;
+	}
+}
---
+++ tests/AccessRouterTest.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+ * This file is part of the miBadger package.
+ *
+ * @author Michael Webbers <michael@webbers.io>
+ * @license http://opensource.org/licenses/Apache-2.0 Apache v2 License
+ */
+
+namespace miBadger\Router;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * The router test class.
+ *
+ * @since 1.0.0
+ */
+class AccessRouterTest extends TestCase
+{
+	/** @var callable The callable. */
+	private $callable;
+
+	/** @var Router The router. */
+	private $router;
+
+	public function setUp()
+	{
+		$this->user = new User();
+		$this->router = new AccessRouter($this->user, '');
+
+		$this->callable = 'miBadger\Router\accessRouterMethodTest';
+		$this->router->add('GET', '/path/', null, $this->callable);
+	}
+
+	public function testResolveMethod()
+	{
+		$this->assertEquals('method', $this->router->resolve('GET', '/path/'));
+	}
+
+	public function testResolvePermission()
+	{
+		// user + correct permission set
+		$this->router->add('GET', '/read/', Permission::READ_ACCESS, function() {
+			return 'access granted!';
+		});
+		$this->assertEquals('access granted!', $this->router->resolve('GET', '/read/'));
+	}
+
+	/**
+	 * @expectedException miBadger\Http\ServerResponseException
+	 * @expectedExceptionCode 401
+	 */
+	public function testAccessDeniedOnResolve()
+	{
+		// user + incorrect permission set
+		$this->router->add('GET', '/delete/', Permission::DELETE_ACCESS, function () {
+			return 'access granted';
+		});
+		$this->router->resolve('GET', '/delete/');
+	}
+
+	/**
+	 * @expectedException miBadger\Http\ServerResponseException
+	 * @expectedExceptionCode 401
+	 */
+	public function testAccessDeniedOnNoUser()
+	{
+		// null user + permission set
+		$noUserRouter = new AccessRouter(null, '');
+		$noUserRouter->add('GET', '/read/', Permission::READ_ACCESS, function() {
+			return 'access granted!';
+		});
+		$noUserRouter->resolve('GET', '/read/');
+	}
+
+	public function testResolveNoPermission()
+	{
+		// User + null permission on route
+		$this->assertEquals('method', $this->router->resolve('GET', '/path/'));
+	}
+
+	public function testResolveNoUserNoPermission()
+	{
+		// null user + null permission on route
+		$noUserRouter = new AccessRouter(null, '');
+		$noUserRouter->add('GET', '/path/', null, $this->callable);
+		$this->assertEquals('method', $noUserRouter->resolve('GET', '/path/'));
+	}
+
+
+	public function testGetPermissionMap()
+	{
+		$this->router->add('GET', '/foo/{id}/', Permission::READ_ACCESS, function ($id) {
+			return $id;
+		});
+
+		$this->assertEquals(['/path/' => null, '/foo/%s/' => [Permission::READ_ACCESS]], $this->router->makeGETRoutePermissionsMap());
+	}
+
+	/**
+	 * @expectedException miBadger\Http\ServerResponseException
+	 * @expectedExceptionMessage Not Found
+	 * @expectedExceptionCode 404
+	 */
+	public function testResolveNotFound()
+	{
+		$this->router->resolve();
+	}
+
+	/**
+	 * @expectedException miBadger\Http\ServerResponseException
+	 * @expectedExceptionMessage Not Found
+	 * @expectedExceptionCode 404
+	 */
+	public function testResolveWildcardNotFound()
+	{
+		$this->router->add('GET', '{name}', null, function($name){ return $name; });
+		$this->router->resolve('GET', '/foo/');
+	}	
+}
+
+class Permission
+{
+	const READ_ACCESS = "READ_ACCESS";
+	const WRITE_ACCESS = "WRITE_ACCESS";
+	const DELETE_ACCESS = "DELETE_ACCESS";
+}
+
+class User implements PermissionCheckable
+{
+	public function hasPermission($permission)
+	{
+		$permissions = [Permission::READ_ACCESS, Permission::WRITE_ACCESS];
+		return in_array($permission, $permissions);
+	}
+}
+
+function accessRouterMethodTest()
+{
+	return 'method';
+}
